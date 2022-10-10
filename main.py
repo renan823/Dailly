02e15e235107b816cf2f73f6142c26949980b0f3
Renan <70537760+renan823@users.noreply.github.com>
2022-10-10 15:51:28 -0300

    add new routes

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import datetime
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import extract
 
 app = Flask(__name__)
 app.secret_key = "hi"
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///dailly.sqlite3'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
 #DB config-----------------------------------------------------------------
-
 class users(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
     name = db.Column(db.String(255))
@@ -31,8 +31,8 @@ class posts(db.Model):
         self.text = text
 
 class follow(db.Model):
-    user = db.Column(db.String(255), db.ForeignKey('users.name'), primary_key=True)
-    follower = db.Column(db.String(255), db.ForeignKey('users.name'), primary_key=True)
+    user = db.Column(db.Integer, db.ForeignKey('users.name'), primary_key=True)
+    follower = db.Column(db.Integer, db.ForeignKey('users.name'), primary_key=True)
 
     def __init__(self, user1, user2):
         self.user = user1
@@ -40,13 +40,47 @@ class follow(db.Model):
 
 class make_post(db.Model):
     _id = db.Column("id", db.Integer, db.ForeignKey('posts.id'), primary_key=True)
-    user = db.Column(db.String(255), db.ForeignKey('users.name'), primary_key=True)
-    date = db.Column(db.DateTime)
+    user = db.Column(db.Integer, db.ForeignKey('users.name'), primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow())
 
-    def __init__(self, user, idP, date):
+    def __init__(self, user, idP):
         self.user = user
         self._id = idP
-        self.date = date
+
+
+#functions------------------------------------------------------------------
+def countFollowers(user):
+    return follow.query.filter_by(follower=user).count()
+
+def countFollowing(user):
+    return follow.query.filter_by(user=user).count()
+
+def searchUser(name):
+    search = "%{}%".format(name)
+    return users.query.filter(users.name.like(search)).all()
+
+def getFollowers(user):
+    return follow.query.filter_by(user=user).all()
+
+def getUsers(req):
+    users = searchUser(req["name"])
+
+    resultFollowers = []
+    followers = getFollowers(session["id"])
+    for follower in followers:
+        resultFollowers.append(follower)
+
+    resultUsers = []
+    for user in users:
+        if user._id != session["id"]:
+            flag = True
+            for follower in followers:
+                if int(user._id) == int(follower.follower):
+                    flag = False
+                    break
+            if flag:
+                resultUsers.append({"name": user.name, "_id": user._id, })
+    return resultUsers
 
 #routes---------------------------------------------------------------------
 @app.route("/", methods=['POST', 'GET'])
@@ -59,6 +93,7 @@ def login():
             if find.password == password:
                 session["user"] = user
                 session["password"] = password
+                session["id"] = find._id
             else:
                 return redirect(url_for("login"))
         else:
@@ -78,6 +113,8 @@ def user():
     if "user" in session:
         user = {}
         user["name"] = session["user"]
+        user["followers"] = countFollowers(session["id"])
+        user["following"] = countFollowing(session["id"])
         return render_template('users.html', user=user)
     return redirect(url_for("login"))
 
@@ -91,29 +128,51 @@ def home():
 def logout():
     session.pop("user", None)
     session.pop("password", None)
-    flash("Get out!", "info")
     return redirect(url_for("login"))
 
 @app.route("/makepost/", methods=['POST'])
 def make():
-    print(request.json)
-    return {"msg": "oi"}
+    req = request.get_json(force=True)
     date = datetime.now()
-    getPost = make_post.query.filter_by(date=date)
+    getPost = make_post.query.filter_by(user=session["id"]).filter(extract('day', make_post.date) == date.day).all()
+    
     if getPost:
-        print("Já fez post hj")
+        return make_response(jsonify({"msg": "Você já fez um post hoje!", "status": "BAD"}), 200)
     else:
-        title = request.form["title"]
-        text = request.form["text"]
-
-        post = posts(title, text)
+        post = posts(req["title"], req["text"])
         db.session.add(post)
         db.session.commit()
 
-        make = make_post(session["user"], post._id, date)
+        make = make_post(session["id"], post._id)
         db.session.add(make)
         db.session.commit()
+
+    return make_response(jsonify({"msg": "Post publicado!", "status": "OK"}), 200)
     
+@app.route("/search/", methods=['POST'])
+def search():
+    req = request.get_json(force=True)
+    resultUsers = getUsers(req)
+    return make_response(jsonify({"users": resultUsers}), 200)
+
+@app.route("/follower/", methods=['POST'])
+def follower():
+    req = request.get_json(force=True)
+
+
+    newFollow = follow(session["id"], req["user"])
+    db.session.add(newFollow)
+    db.session.commit()
+
+    return make_response(jsonify({"status": "OK"}), 200)
+
+@app.route("/user/followers/", methods=['POST'])
+def getUserFollowers():
+    followers = countFollowers(session["id"])
+    following = countFollowing(session["id"])
+
+    return make_response(jsonify({"followers": followers, "following": following, "status": "OK"}), 200)
+
 
 #debug mode-----------------------------------------------------------------
 if __name__ == '__main__':
