d18bea3c0959b2096dda887baceac6439db26c13
Renan <70537760+renan823@users.noreply.github.com>
2022-10-15 14:19:27 -0300

    Update getPosts.js

--- static/scripts/getPosts.js
+++ static/scripts/getPosts.js
@@ -13,7 +13,31 @@ const getPosts =  ()=>{
         return response.json()
     })
     .then(data=>{
-        results.innerHTML = ""
+        postList = data.posts
+        for(let i in postList){
+            let posts = postList[i]
+            for(let j in posts){
+                let post = posts[j]
+                
+                let box = document.createElement("div")
+                box.classList.add("post")
+
+                let userName = document.createElement("h5")
+                userName.innerText = post.user
+                box.appendChild(userName)
+
+
+                let title = document.createElement("h2")
+                title.innerText = post.title
+                box.appendChild(title)
+
+                let text = document.createElement("p")
+                text.innerText = post.text
+                box.appendChild(text)
+
+                results.appendChild(box)
+            }
+        }
     })
 }
 
